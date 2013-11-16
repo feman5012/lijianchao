1cf7b15e29d82e29f83200363e13b5d4cb6ff2bc
me <me@me-Aspire-4745G.(none)>
2013-11-16 16:46:26 +0800

    another method to rename file

--- src/catfolderfilename.bash
+++ src/catfolderfilename.bash
@@ -1,80 +1,24 @@
 #! /bin/bash
 
-IFS=:
-declare -a arr1
-declare -a arr2
-declare -a arr3
-declare -a arr4
+if [[ "x$1" == "x" ]]; then
+	echo "usage: rename path";
+fi
 
-d_work=$1
-f_path="1.txt"
-f_new_name="2.txt"
-f_new_file="3.txt"
-f_old_file="4.txt"
+f_result=$$.txt
 
-#echo $d_work
+find $1 -type f > $f_result
 
-# old path
+while read line; do
+	l_no=${line#./}
+	l_dir=${l_no%/*}/
+	#echo $l_no $l_dir; exit
+	l_newname=$(echo $l_no | sed 's/\//_/g')
+	#echo $l_newname; exit
+	l_newfile=$l_dir$l_newname
+	#echo $l_newfile; exit
+	mv "$line" "$l_newfile"
+done < $f_result
 
-find $d_work -type f | sed  -e 's/\(\..*\/\).\+/\1/' > $f_path
-
-while read line
-do
-    arr1=("${arr1[@]}" $line)
-done < $f_path
-
-#echo ${arr1[*]}
-
- 
-# new name
-find $d_work -type f | sed  -e 's/\.\/\(.*\)/\1/' -e 's/\//_/g'  > $f_new_name
-
-
-
-while read line
-do
-    arr2=("${arr2[@]}" $line)
-done < $f_new_name
-
-#echo ${arr2[*]}
-
-
-# cat p-ath and name
-
-
-rm -f $f_new_file
-
-len=${#arr2[*]}
-for ((i=0; i < len; i++)); do
-	echo -n ${arr1[i]}
-	echo ${arr2[i]}; 
-done >> $f_new_file
-
-
-
-# old file
-
-find $d_work -type f > $f_old_file
-
-while read line
-do
-    arr3=("${arr3[@]}" $line)
-done < $f_old_file
-
-
-# move file
-while read line
-do
-    arr4=("${arr4[@]}" $line)
-done < $f_new_file
-
-for ((i=0; i < len; i++)); do
-	mv "${arr3[i]}"  "${arr4[i]}" 
-done 
-
-
-
-
-
+rm -f $f_result
 
 
