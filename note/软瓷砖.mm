9ec147bc1526a1526a20005595fd9a28092bbab8
me <me@me-Aspire-4745G.(none)>
2014-06-15 10:20:43 +0800

    move home

---
+++ note/软瓷砖.mm
@@ -0,0 +1,16 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1402150114541" ID="ID_1321466538" MODIFIED="1402150267676" TEXT="0">
+<node CREATED="1402150271187" HGAP="30" ID="ID_123325037" MODIFIED="1402150278429" POSITION="right" TEXT="&#x6982;&#x5ff5;" VSHIFT="-53">
+<node CREATED="1402150280075" HGAP="31" ID="ID_615800155" MODIFIED="1402150283170" TEXT="http://baike.baidu.com/view/3394878.htm?fr=aladdin" VSHIFT="-39"/>
+</node>
+<node CREATED="1402150325130" ID="ID_1486807624" MODIFIED="1402156568340" POSITION="right" TEXT="&#x5382;&#x5bb6;">
+<node CREATED="1402156256948" HGAP="21" ID="ID_1762042950" MODIFIED="1402156286073" TEXT=" &#x4f5b;&#x5c71;&#x5e02;&#x5609;&#x6b23;&#x534e;" VSHIFT="-21">
+<node CREATED="1402156246148" HGAP="26" ID="ID_1648899788" MODIFIED="1402156249034" TEXT="http://detail.1688.com/offer/36292999928.html" VSHIFT="-19"/>
+</node>
+<node CREATED="1402156419477" ID="ID_494297164" MODIFIED="1402156442294" TEXT=" &#x4e1c;&#x839e;&#x5e02;&#x5858;&#x53a6;&#x6da6;&#x5229;&#x9676;&#x74f7;&#x5546;&#x884c; ">
+<node CREATED="1402156422540" HGAP="27" ID="ID_1969594857" MODIFIED="1402156445739" TEXT="http://detail.1688.com/offer/1186208804.html" VSHIFT="-26"/>
+</node>
+</node>
+</node>
+</map>
